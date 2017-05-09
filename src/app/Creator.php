9cbdbac5722d51fb9ff557e51e0f3a0e8bbf30a9
jibo <jibo@outlook.com>
2017-05-09 16:42:20 +0800

    Build expOrderInfo

--- src/app/Creator.php
+++ src/app/Creator.php
@@ -3,6 +3,7 @@
 namespace app;
 
 use data\Database;
+use data\OrderDB;
 use DB\SQL\Mapper;
 use Httpful\Mime;
 use Ramsey\Uuid\Uuid;
@@ -11,6 +12,23 @@ class Creator
 {
     function create($f3)
     {
+        $odb = OrderDB::mysql();
+
+        $order = new Mapper($odb, 'order_item');
+        $order->load(['trace_id = ?', $_REQUEST['id']]);
+
+        if ($order->dry()) {
+            goto NOT_FOUND;
+        }
+
+        $model = new Mapper($odb, 'prototype');
+        $model->load(['ID = ?', $order['prototype_id']]);
+
+        $contact = new Mapper($odb, 'distribution');
+        $contact->load(['ID = ?', $order['distribution_id']]);
+
+        $this->buildOrderInfo($f3, $order, $model, $contact);
+
         $f3->set('guid', Uuid::uuid1());
         $f3->set('expOrderInfo', $this->expOrderInfo);
         $f3->set('expInventoryHead', $this->expInventoryHead);
@@ -26,13 +44,34 @@ class Creator
         $mapper['xml'] = $xml;
         $mapper->save();
 
-        //header('Content-Type:application/xml');
-        //echo $xml;
-        $response = \Httpful\Request::post($f3->get('DECLARE_URL'))
+        header('Content-Type:application/xml');
+        echo $xml;
+
+        /*$response = \Httpful\Request::post($f3->get('DECLARE_URL'))
             ->body($xml)
             ->sendsType(Mime::FORM)
             ->send();
-        var_dump($response);
+        var_dump($response);*/
+
+        NOT_FOUND:
+        return header('HTTP/1.1 404 Not Found');
+    }
+
+    function buildOrderInfo($f3, $order, $model, $contact)
+    {
+        $this->expOrderInfo['orderNo'] = $order['trace_id'];
+        $this->expOrderInfo['payTime'] = date('YmdHis', strtotime($order['create_time']));
+        $this->expOrderInfo['goodsNum'] = 1;
+        $this->expOrderInfo['currency'] = 142; // 人民币
+        $this->expOrderInfo['rate'] = 1;
+        $this->expOrderInfo['orderTotalAmount'] = $order['price'] ? $order['price'] : $model['cost'];
+        $this->expOrderInfo['consignee'] = $contact['name'];
+        $this->expOrderInfo['consigneeCountry'] = $contact['country'];
+        $this->expOrderInfo['consigneeAddress'] = $contact['address'] . ' ' . $contact['city'] . ' ' . $contact['state'];
+        $this->expOrderInfo['consigneeTel'] = $contact['phone'];
+        $this->expOrderInfo['consigneeEmail'] = $contact['email'];
+        $this->expOrderInfo['ebpCode'] = $f3->get('EBPCODE');
+        $this->expOrderInfo['ebpName'] = $f3->get('EBPNAME');
     }
 
     private $expOrderInfo = [
