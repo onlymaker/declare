59c4930c31cfeffc7019948b2ab5fb5323bb6f77
jibo <jibo@outlook.com>
2017-05-16 17:54:27 +0800

    Check result before save

--- src/app/Receiver.php
+++ src/app/Receiver.php
@@ -16,24 +16,38 @@ class Receiver
 
     function parseResult($xml)
     {
-        $parser = xml_parser_create();
-        xml_parse_into_struct($parser, $xml, $result, $index);
+        $f3 = \Base::instance();
+        try {
+            $parser = xml_parser_create();
+            xml_parse_into_struct($parser, $xml, $result, $index);
 
-        $guid = $result[$index['CEBEXPINVTRETURNMESSAGE'][0]]['attributes']['GUID'];
-        $returnGuid = $result[$index['GUID'][0]]['value'];
-        $returnTime = $result[$index['RETURNTIME'][0]]['value'];
-        $returnInfo = $result[$index['RETURNINFO'][0]]['value'];
-        $returnStatus = $result[$index['RETURNSTATUS'][0]]['value'];
+            $guid = $result[$index['CEBEXPINVTRETURNMESSAGE'][0]]['attributes']['GUID'];
+            $returnGuid = $result[$index['GUID'][0]]['value'];
+            $returnTime = $result[$index['RETURNTIME'][0]]['value'];
+            $returnInfo = $result[$index['RETURNINFO'][0]]['value'];
+            $returnStatus = $result[$index['RETURNSTATUS'][0]]['value'];
 
-        $mapper = new Mapper(Database::mysql(), 'notify');
-        $mapper['guid'] = $guid;
-        $mapper['return_guid'] = $returnGuid;
-        $mapper['return_time'] = $returnTime;
-        $mapper['return_info'] = $returnInfo;
-        $mapper['return_status'] = $returnStatus;
-        $mapper['xml'] = $xml;
-        $mapper->save();
+            $mapper = new Mapper(Database::mysql(), 'notify');
+            $mapper->load(['return_guid = ?', $returnGuid]);
+            if ($mapper->dry()) {
+                $mapper['guid'] = $guid;
+                $mapper['return_guid'] = $returnGuid;
+                $mapper['return_time'] = $returnTime;
+                $mapper['return_info'] = $returnInfo;
+                $mapper['return_status'] = $returnStatus;
+                $mapper['xml'] = $xml;
+                $mapper->save();
+            } else {
+                $f3->log('Existed: ' . $mapper['xml']);
+            }
 
-        return $mapper;
+            return $mapper;
+        } catch (\Exception $e) {
+            return [
+                'return_info' => $e->getMessage(),
+                'return_status' => $e->getCode(),
+                'xml' => $e->getTraceAsString()
+            ];
+        }
     }
 }
